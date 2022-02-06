b20619ddd9c732127e645b7d79fe35d7e97816ce
Daemon-L <lit.dmitriy.ua@gmail.com>
2022-02-06 23:12:09 +0200

    Начало рефакторинга кода

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,45 +1,42 @@
-import { Component } from 'react';
+// import { Component } from 'react';
+import { useState } from "react";
 import PropTypes from 'prop-types';
 import { FormContainer, Text, Input, Button } from './Form.styled'
 
-class ContactForm extends Component { 
-
-    state = {
-        name: '',
-        number: '',
+export default function ContactForm({onSubmit}) { 
+    const [name, setName] = useState('');
+    const [number, setNumber] = useState('');
+   
+    const handleChange = evt => {
+        const formField = evt.currentTarget.name;
+        if (formField === 'name') {
+            setName(evt.currentTarget.value);
+        }
+        if (formField === 'number') {
+            setNumber(evt.currentTarget.value);
+        }
     };
 
-    handleChange = evt => {
-        const { name, value } = evt.currentTarget;
-        this.setState({
-            [name]: value,
-        });
-    };
-
-    handleSubmit = evt => {
+    const handleSubmit = evt => {
         evt.preventDefault();
-        this.props.onSubmit(this.state);
-        this.reset();
+        onSubmit(name, number);
+        reset();
     };
 
-    reset = () => {
-        this.setState({ name: '', number: '' });
+    const reset = () => {
+        setName('');
+        setNumber('');
     };
 
-    static propTypes = {
-        onSubmit: PropTypes.func.isRequired,
-    };
-
-    render() { 
         return (
-            <FormContainer onSubmit={this.handleSubmit}>
+            <FormContainer onSubmit={handleSubmit}>
                 <label>
                     <Text>Name</Text>
                     <Input
                         type="text"
                         name="name"
-                        value={this.state.name}
-                        onChange={this.handleChange}
+                        value={name}
+                        onChange={handleChange}
                         pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
                         title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
                         required
@@ -50,8 +47,8 @@ class ContactForm extends Component {
                     <Input
                         type="tel"
                         name="number"
-                        value={this.state.number}
-                        onChange={this.handleChange}
+                        value={number}
+                        onChange={handleChange}
                         pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
                         title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
                         required
@@ -60,11 +57,61 @@ class ContactForm extends Component {
                 <Button type="submit">Add contact</Button>
             </FormContainer>
         );
-    }
 }
 
-// ContactForm.propTypes = {
-//   onSubmit: PropTypes.func.isRequired,
-// };
+ContactForm.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};
 
-export default ContactForm;
+// class ContactForm extends Component { 
+//     state = {
+//         name: '',
+//         number: '',
+//     };
+//     handleChange = evt => {
+//         const { name, value } = evt.currentTarget;
+//         this.setState({
+//             [name]: value,
+//         });
+//     };
+//     handleSubmit = evt => {
+//         evt.preventDefault();
+//         this.props.onSubmit(this.state);
+//         this.reset();
+//     };
+//     reset = () => {
+//         this.setState({ name: '', number: '' });
+//     };
+//     render() { 
+//         return (
+//             <FormContainer onSubmit={this.handleSubmit}>
+//                 <label>
+//                     <Text>Name</Text>
+//                     <Input
+//                         type="text"
+//                         name="name"
+//                         value={this.state.name}
+//                         onChange={this.handleChange}
+//                         pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+//                         title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+//                         required
+//                     />  
+//                 </label>
+//                 <label>
+//                     <Text>Number</Text>
+//                     <Input
+//                         type="tel"
+//                         name="number"
+//                         value={this.state.number}
+//                         onChange={this.handleChange}
+//                         pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+//                         title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+//                         required
+//                     />
+//                 </label>
+//                 <Button type="submit">Add contact</Button>
+//             </FormContainer>
+//         );
+//     }
+// }
+// export default ContactForm;
--- src/components/ContactList/ContactListItem.jsx
+++ src/components/ContactList/ContactListItem.jsx
@@ -3,14 +3,26 @@ import PropTypes from 'prop-types';
 import { ContactItem, Button, Text } from './ContactList.styled'
 
 const ContactListItem = ({ contactsItem, onDeleteContact }) =>
-  contactsItem.map(({ id, name, number }) => (
-    <ContactItem key={id}>
-      <Text>{name}: {number}</Text>
-      <Button onClick={() => onDeleteContact(id)}>
+  contactsItem.map(contact => (
+    <ContactItem key={contact.id}>
+      <Text>{contact.name}: {contact.number}</Text>
+      <Button onClick={() => onDeleteContact(contact.id)}>
         Delete
       </Button>
     </ContactItem>
-  ));
+  )
+);
+
+//   const ContactListItem = ({ contactsItem, onDeleteContact }) =>
+//   contactsItem.map(({ id, name, number }) => (
+//     <ContactItem key={id}>
+//       <Text>{name}: {number}</Text>
+//       <Button onClick={() => onDeleteContact(id)}>
+//         Delete
+//       </Button>
+//     </ContactItem>
+//   )
+// );
 
 ContactListItem.propTypes = {
     contacts: PropTypes.arrayOf(
@@ -24,18 +36,3 @@ ContactListItem.propTypes = {
 };
 
 export default ContactListItem;
-
-// const ContactList = ({ contacts, onDeleteContact }) => (
-//     <ul>
-//         {contacts.map(({ id, name, number }) => (
-//             <ContactItem key={id}>
-//                 <Text>{name}: {number}</Text>
-//                 <Button type="button"
-//                         onClick={() => onDeleteContact(id)}>
-//                     Delete
-//                 </Button>
-//             </ContactItem>
-//         ))}
-//     </ul>
-// );
-// export default ContactList;
