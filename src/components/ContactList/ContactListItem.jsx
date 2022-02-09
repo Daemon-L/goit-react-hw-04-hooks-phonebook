c20eb6b33d9e3322815efe0fbab51934e5925df1
Daemon-L <lit.dmitriy.ua@gmail.com>
2022-02-09 21:04:21 +0200

    Исправляет ошибку в форме

--- src/components/ContactList/ContactListItem.jsx
+++ src/components/ContactList/ContactListItem.jsx
@@ -1,36 +1,24 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import { ContactItem, Button, Text } from './ContactList.styled'
 
 const ContactListItem = ({ contact: {id, name, number }, onDeleteContact }) => {
   return <ContactItem>
-      <Text>{name}: {number}</Text>
-      <Button onClick={() => onDeleteContact(id)}>
-        Delete
-      </Button>
-    </ContactItem>
+    <Text>{name}: {number}</Text>
+    <Button onClick={() => onDeleteContact(id)}>
+      Delete
+    </Button>
+  </ContactItem>
 }
 export default ContactListItem;
 
-ContactListItem.propTypes = {
-    contacts: PropTypes.arrayOf(
-        PropTypes.shape({
-            id: PropTypes.string.isRequired,
-            name: PropTypes.string.isRequired,
-            number: PropTypes.string.isRequired,
-        })
-    ),
-    onDeleteContact: PropTypes.func.isRequired,
-};
-
-// **********************************************
-// const ContactListItem = ({ contactsItem, onDeleteContact }) =>
-//   contactsItem.map(({ id, name, number }) => (
-//     <ContactItem key={id}>
-//       <Text>{name}: {number}</Text>
-//       <Button onClick={() => onDeleteContact(id)}>
-//         Delete
-//       </Button>
-//     </ContactItem>
-//   )
-//   );
+// ContactListItem.propTypes = {
+//   contacts: PropTypes.arrayOf(
+//       PropTypes.shape({
+//           id: PropTypes.string.isRequired,
+//           name: PropTypes.string.isRequired,
+//           number: PropTypes.string.isRequired,
+//       })
+//   ),
+//   onDeleteContact: PropTypes.func.isRequired,
+// };
--- src/components/ContactList/ConyactList.jsx
+++ src/components/ContactList/ConyactList.jsx
@@ -1,35 +1,29 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import ContactListItem from './ContactListItem';
 
 const ContactList = ({ contacts, onDeleteContact }) => (
-        <ul>
+    <ul>
         {contacts.map(contact =>
             <ContactListItem
                 key={contact.id}
                 contact={contact}
                 onDeleteContact={onDeleteContact}
             />
+            
         )}
     </ul>
+    
 );
 export default ContactList;
 
-ContactList.propTypes = {
-    contacts: PropTypes.arrayOf(
-        PropTypes.shape({
-            id: PropTypes.string.isRequired,
-            name: PropTypes.string.isRequired,
-            number: PropTypes.string.isRequired,
-        })
-    ),
-    onDeleteContact: PropTypes.func.isRequired,
-};
-
-// ***************************************************
-// const ContactList = ({ contacts, onDeleteContact }) => (
-//     <ul>
-//         <ContactListItem contactsItem={contacts} onDeleteContact={onDeleteContact} />
-//     </ul>
-// );
-
+// ContactList.propTypes = {
+//     contacts: PropTypes.arrayOf(
+//         PropTypes.shape({
+//             id: PropTypes.string.isRequired,
+//             name: PropTypes.string.isRequired,
+//             number: PropTypes.string.isRequired,
+//         })
+//     ),
+//     onDeleteContact: PropTypes.func.isRequired,
+// };
